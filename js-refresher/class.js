658747a6d953335784ff501444db634c671c4070
Bryan C Guner <bryan.guner@gmail.com>
2023-01-26 18:43:57 +0000

    m update

--- js-refresher/class.js
+++ js-refresher/class.js
@@ -1,24 +1,39 @@
 class Human {
-    constructor () {
-        this.gender = 'male';
-    }
-    printGender() {
-        console.log(this.gender)
-    }
+  constructor() {
+    this.gender = "female";
+  }
+  printGender() {
+    console.log(this.gender);
+  }
 }
-
-
-
 class Person extends Human {
-    constructor () {
-        super();
-        this.name = "Bryan"
-        this.gender = "female"
-    }
-    printMyName() {
-        console.log(this.name)
-    }
+  constructor() {
+    super();
+    this.name = "Bryan";
+    this.gender = "male";
+  }
+  printMyName() {
+    console.log(this.name);
+  }
 }
 const person = new Person();
 person.printMyName();
 person.printGender();
+//es 7 syntax
+class Human1 {
+     gender = "female";
+    printGender=()=> {
+      console.log(this.gender);
+    }
+  }
+  class Person1 extends Human {
+name = "Bryan";
+gender = "male";
+  
+    printMyName=() =>{
+      console.log(this.name);
+    }
+}
+  const person1 = new Person1();
+  person1.printMyName();
+  person1.printGender();
