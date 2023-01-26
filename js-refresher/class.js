8c63c6c28c05f175815112405b840ac621634705
Bryan C Guner <bryan.guner@gmail.com>
2023-01-26 18:33:50 +0000

    m update

--- js-refresher/class.js
+++ js-refresher/class.js
@@ -1,8 +1,24 @@
-class Person {
+class Human {
     constructor () {
-      this.name = "Bryan"
+        this.gender = 'male';
+    }
+    printGender() {
+        console.log(this.gender)
+    }
+}
+
+
+
+class Person extends Human {
+    constructor () {
+        super();
+        this.name = "Bryan"
+        this.gender = "female"
     }
     printMyName() {
         console.log(this.name)
     }
 }
+const person = new Person();
+person.printMyName();
+person.printGender();
