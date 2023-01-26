53daf61c7bd4fe08e353838e845d38aabdb703ce
Bryan C Guner <bryan.guner@gmail.com>
2023-01-26 16:33:19 +0000

    m update

--- js-refresher/arrow-functions.js
+++ js-refresher/arrow-functions.js
@@ -23,3 +23,19 @@ Arrow functions cannot be used as constructors. Calling them with new throws a T
 Arrow functions cannot use yield within their body and cannot be created as generator functions.
 
 */
+const materials = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+  
+  console.log(materials.map(material => material.length));
+  // Expected output: Array [8, 6, 7, 9]
+
+  
+  
+  // with arrow functions you can omit the parentheses if you are only passing one parameter to the function
+  
+const myName = name => { return name };
+console.log(myName('Bryan'))
