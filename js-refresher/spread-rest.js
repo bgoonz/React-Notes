bf137e778f1dbf0ba76b8dab5e04fe4eab0560c0
Bryan C Guner <bryan.guner@gmail.com>
2023-01-26 19:37:29 +0000

    m update

--- js-refresher/spread-rest.js
+++ js-refresher/spread-rest.js
@@ -5,3 +5,10 @@ const numbers = [1, 2, 3, 4, 5];
 const newNumbers = [...numbers, 6];
 
 console.log(newNumbers);
+
+
+
+const filter = ( ...args ) => {
+    return args.filter (el => el ===1);
+}
+console.log(filter(1,2,3,4))
