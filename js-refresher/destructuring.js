d89beaa0ce27a2b06e00d52d79659ed33d5855dd
Bryan C Guner <bryan.guner@gmail.com>
2023-01-26 19:44:25 +0000

    m update

---
+++ js-refresher/destructuring.js
@@ -0,0 +1,16 @@
+//Destructuring lets you extract individual array elements or object properties and store them in variables
+[a,b]=["hello", "Bryan"]
+console.log(a,b)
+
+const numbers = [ 1, 2, 3 ];
+[num1,num2]=numbers
+console.log( num1, num2 );
+[ num1, , num3 ] = numbers
+console.log( num1, num3 );
+
+
+
+//Object Destructuring
+
+
+
