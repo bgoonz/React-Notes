4950a4c057a46526697d69392fb47f82f302b56c
Bryan C Guner <bryan.guner@gmail.com>
2023-01-27 21:41:47 +0000

    m update

--- 03react-components/code/01-starting-setup/src/index.js
+++ 03react-components/code/01-starting-setup/src/index.js
@@ -1,7 +1,10 @@
-import ReactDOM from "react-dom/client";
 import React from "react";
+import ReactDOM from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
-
+// this is the first file to exicute
+// react is split across two seperate libraries... react & react-Dom
+//
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
