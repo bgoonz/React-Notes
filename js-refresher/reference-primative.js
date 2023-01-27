a7874b5ac0c693a0c6878eb96b0df6c774a787cf
Bryan C Guner <bryan.guner@gmail.com>
2023-01-27 20:13:52 +0000

    m update

--- js-refresher/reference-primative.js
+++ js-refresher/reference-primative.js
@@ -7,6 +7,17 @@ const person = {
 }
 const secondPerson = person;
 console.log( secondPerson );//{ name: 'Bryan' } not an actual copy of person... secondPerson is a pointer to the original object pointed to by the person variable.
+
+
+
+// To actually copy an object use the spread operator
+const personCopy = {
+    ...person
+}
+
 person.name = 'nick'
 console.log( secondPerson );//{ name: 'nick' } changing the name property on the original object also changed the second person object
+console.log(personCopy)
+
+
 
