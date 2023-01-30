645a56abbb54fad4d6650335039d3951c9695be3
Bryan C Guner <bryan.guner@gmail.com>
2023-01-30 16:16:13 +0000

    m update

--- 03react-components/code/01-starting-setup/src/App.js
+++ 03react-components/code/01-starting-setup/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+// JSX is short for javascript xml (html is a kind of xml?)
 function App() {
+    // we return html code inside of a javascript file... this is not valid js code ... it is JSX which is a special syntax invented by the react team
   return (
     <div>
       <h2>Let's get started!</h2>
--- 03react-components/code/01-starting-setup/src/index.js
+++ 03react-components/code/01-starting-setup/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import App from "./App";
 // this is the first file to exicute
 // react is split across two seperate libraries... react & react-Dom
-//
+// if you import a function in file a that you want to use in file b then you need to export it from file a....
+// the line below 'createRoot' is the main hook and tells the webpage where you want to put the entry point of the react application
+//corresponds to the div with id='root' in the index.html where we want to inject our React UI ... document.getElementById("root"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App />);// render method tells react what should be rendered on the root element
