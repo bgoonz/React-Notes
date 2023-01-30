d88e860f61aca0f8c0660737c849da30e48138d4
Bryan C Guner <bryan.guner@gmail.com>
2023-01-30 16:20:38 +0000

    m update

--- 03react-components/code/01-starting-setup/src/App.js
+++ 03react-components/code/01-starting-setup/src/App.js
@@ -4,7 +4,14 @@ function App() {
     // we return html code inside of a javascript file... this is not valid js code ... it is JSX which is a special syntax invented by the react team
   return (
     <div>
-      <h2>Let's get started!</h2>
+          <h2>Let's get started!</h2>
+          <p>This is also visable!</p>
+          
+          // In regular javascript you would use an imperative approach
+          //document.getElementById('root')
+          // const pararaph = document.createElement('p')
+          // paragraph.textContent = 'this is also visible'
+          //document.getElementById('root').append(paragraph)
     </div>
   );
 }
