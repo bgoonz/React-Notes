fb0ee2f9875022ddccd478e1fe859a6792babf06
Bryan C Guner <bryan.guner@gmail.com>
2023-01-30 16:33:59 +0000

    m update

--- 03react-components/code/01-starting-setup/src/App.js
+++ 03react-components/code/01-starting-setup/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
 // JSX is short for javascript xml (html is a kind of xml?)
 function App() {
   // we return html code inside of a javascript file... this is not valid js code ... it is JSX which is a special syntax invented by the react team
+  // In regular javascript you would use an imperative approach
+  //document.getElementById('root')
+  // const pararaph = document.createElement('p')
+  // paragraph.textContent = 'this is also  visible'
+  //document.getElementById('root').append(paragraph)
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
       <p>This is also visable!</p>
-      // In regular javascript you would use an imperative approach
-      //document.getElementById('root') // const pararaph =
-      document.createElement('p') // paragraph.textContent = 'this is also
-      visible' //document.getElementById('root').append(paragraph)
     </div>
   );
 }
---
+++ 03react-components/code/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,6 @@
+// a component in react is just a javascript function
+import React from "react";
+function ExpenseItem() {
+  return <h2>Expense Item!</h2>;
+}
+export default ExpenseItem;
