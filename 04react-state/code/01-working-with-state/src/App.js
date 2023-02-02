14907e39e4519f07a89028bc932a557da4f2cfdb
Bryan C Guner <bryan.guner@gmail.com>
2023-02-02 12:07:55 -0500

    imported ExpenseForm to App.js

--- 04react-state/code/01-working-with-state/src/App.js
+++ 04react-state/code/01-working-with-state/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -34,7 +34,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+     <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
