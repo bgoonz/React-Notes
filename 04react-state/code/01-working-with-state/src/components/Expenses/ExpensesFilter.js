c04c55bafd91384f56ef90984aca21ded7abc0be
Bryan C Guner <bryan.guner@gmail.com>
2023-02-03 15:54:38 -0500

    expenses filter and clean up

--- 04react-state/code/01-working-with-state/src/components/Expenses/ExpensesFilter.js
+++ 04react-state/code/01-working-with-state/src/components/Expenses/ExpensesFilter.js
@@ -1,13 +1,16 @@
 import React from "react";
-
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+    const filterChangeHandler = ( event ) => {
+        props.onChangeFilter( event.target.value );
+        console.log( "dropdownChangeHandler: event.target.value:", event.target.value)
+    };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select onChange={filterChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
