17eadfdabb1d5839ff3c0c40fd0bb6fd2f5c7c82
Bryan C Guner <bryan.guner@gmail.com>
2023-02-06 13:29:42 -0500

    hid ad expense form behind add expense button and added a cancel button to return to hidden form state

--- 05rendering-conditional-lists/00-initial-code/src/components/NewExpense/NewExpense.js
+++ 05rendering-conditional-lists/00-initial-code/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = (props) => {
+const NewExpense = ( props ) => {
+    const [ isEditing, setIsEditing ] = useState( false );
+    
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,10 +12,16 @@ const NewExpense = (props) => {
     };
     props.onAddExpense(expenseData);
   };
-
+    const startEditingHandler = () => { 
+        setIsEditing( true );
+    }
+    const stopEditingHandler = () => { //props.onCancel in the expense form
+        setIsEditing( false );
+    }
   return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <div className="new-expense">
+          {!isEditing? <button onClick={startEditingHandler }>Add New Expense</button>:
+              <ExpenseForm onSaveExpenseData={ saveExpenseDataHandler } onCancel={stopEditingHandler } /> }
     </div>
   );
 };
