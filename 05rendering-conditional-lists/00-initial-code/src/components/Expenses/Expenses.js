0f460da1b30d0d5803030527844ae7a014d91c62
Bryan C Guner <bryan.guner@gmail.com>
2023-02-06 12:59:38 -0500

    ExpensesList conditional logic

--- 05rendering-conditional-lists/00-initial-code/src/components/Expenses/Expenses.js
+++ 05rendering-conditional-lists/00-initial-code/src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
+
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -16,18 +17,7 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
 
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
 
   return (
     <div>
@@ -36,7 +26,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
--- 05rendering-conditional-lists/00-initial-code/src/components/Expenses/ExpensesList.js
+++ 05rendering-conditional-lists/00-initial-code/src/components/Expenses/ExpensesList.js
@@ -1,13 +1,22 @@
 import './ExpensesList.css'
+import ExpenseItem from './ExpenseItem';
 
 const ExpensesList = (props) => {
-    
-    
-    
-    return (
-        <ul className="expenses-list">
-            
-        </ul>
-    );
+  let expensesContent = <p>No expenses found.</p>;
+  //      if (filteredExpenses.length > 0) {
+  if (props.items.length > 0) {
+    expensesContent = props.items.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
+    return <ul className="expenses-list">
+      {expensesContent}
+  </ul>;
 }
 export default ExpensesList;
