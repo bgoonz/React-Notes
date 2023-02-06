a7e192adedd974ce5bc61053f88b97f2339edacc
Bryan C Guner <bryan.guner@gmail.com>
2023-02-06 13:05:25 -0500

    expense item wrapped in li

--- 05rendering-conditional-lists/00-initial-code/src/components/Expenses/ExpensesList.js
+++ 05rendering-conditional-lists/00-initial-code/src/components/Expenses/ExpensesList.js
@@ -2,21 +2,23 @@ import './ExpensesList.css'
 import ExpenseItem from './ExpenseItem';
 
 const ExpensesList = (props) => {
-  let expensesContent = <p>No expenses found.</p>;
+//   let expensesContent = <p>No expenses found.</p>;
   //      if (filteredExpenses.length > 0) {
-  if (props.items.length > 0) {
-    expensesContent = props.items.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
+  if (props.items.length === 0) {
+   return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
 
-    return <ul className="expenses-list">
-      {expensesContent}
-  </ul>;
+    return (
+      <ul className="expenses-list">
+            { props.items.map( ( expense ) => (
+                <ExpenseItem
+                    key={ expense.id }
+                    title={ expense.title }
+                    amount={ expense.amount }
+                    date={ expense.date }
+                />
+            ) ) }
+      </ul>
+    );
 }
 export default ExpensesList;
