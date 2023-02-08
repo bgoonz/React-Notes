31b07bdfa81b555345dcec9cf9d73e0e472a1669
Bryan C Guner <bryan.guner@gmail.com>
2023-02-08 13:54:32 -0500

    course input using css modules

--- 06styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 06styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,42 +1,8 @@
 import React, { useState } from "react";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
-import styled from "styled-components";
-//-------------------------------Form Control Component--------------------------------------
-const FormControl = styled.div`
-  margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")};
-  }
+import styles from './CourseInput.module.css';
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")}};
-    background: ${(props) => (props.invalid ? "#fad0ec" : "transparent")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-
-  /* the following applies to input elements that have class name form-control and invalid*/
-  &.invalid input {
-    border-color: red;
-    background: #fad0ec;
-  }
-
-
-`;
 
 //--------------------------------Course Input Component--------------------------------------
 const CourseInput = (props) => {
@@ -60,7 +26,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-          <FormControl invalid={!isValid }>
+          <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input
           style={{
@@ -70,7 +36,7 @@ const CourseInput = (props) => {
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- 06styling-components/src/components/UI/Button/Button.js
+++ 06styling-components/src/components/UI/Button/Button.js
@@ -3,40 +3,40 @@
 // this is called a tagged template literal
 //button is a method on the styled object which we imported from styled-components
 // styled-components has methods for all the html elements i.e. styled.div, styled.button, styled.h1, etc.
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+// 
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+//   &:focus {
+//     outline: none;
+//   }
+// 
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 import React from 'react';
 import styles from "./Button.module.css";
-const Button = styled.button`
-    width: 100%; 
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-  
-  @media (min-width: 768px) {
-    width: auto;
-    
-  }
-  &:focus {
-    outline: none;
-  }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
 
-// const Button = (props) => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
