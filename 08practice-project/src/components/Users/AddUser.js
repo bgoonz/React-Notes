12065f5f061ed6961f6597e694018e7357c5ca27
Bryan C Guner <bryan.guner@gmail.com>
2023-02-09 11:54:56 -0500

    <div className={ }> to apply styles from module on custom card component

--- 08practice-project/src/components/Users/AddUser.js
+++ 08practice-project/src/components/Users/AddUser.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 
-const AddUser = ( props ) => {
-    const addUserHndler = ( event ) => {
-        event.preventDefault();
-    };
-    
-    return (
-        <form onSubmit={addUserHndler }>
+const AddUser = (props) => {
+  const addUserHndler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHndler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text"></input>
         <label htmlFor="age">Age (years)</label>
-            <input id="age" type="number"></input>
-            <button type="submit">Add User</button>
+        <input id="age" type="number"></input>
+        <button type="submit">Add User</button>
       </form>
-    );
-    
-}
+    </Card>
+  );
+};
 
 export default AddUser;
