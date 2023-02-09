e4ef8278f4e5fc24fd81690481e271b90f655178
Bryan C Guner <bryan.guner@gmail.com>
2023-02-09 13:23:30 -0500

    UsersList component

---
+++ 08practice-project/src/components/Users/UsersList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../UI/Card";
+import styles from "./UsersList.module.css";
+const UsersList = (props) => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.name} ({user.age} years old)
+            </li>
+          );
+        })}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
