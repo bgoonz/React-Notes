5504ff56e5a8a2590ff1fde5242c1bfd5e2b6100
Bryan C Guner <bryan.guner@gmail.com>
2023-02-09 14:08:06 -0500

    ErrorModal markup setup and css module

---
+++ 08practice-project/src/components/UI/ErrorModal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import styles from "./ErrorModal.module.css";
+
+const ErrorModal = (props) => {
+  return (
+    <Card>
+      <header>
+        <h2></h2>
+      </header>
+      <div>
+        <p></p>
+          </div>
+          <footer>
+            <Button></Button>
+          </footer>
+          
+    </Card>
+  );
+};
+
+export default ErrorModal;
