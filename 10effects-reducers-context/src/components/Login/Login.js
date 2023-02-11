a76a6ac07f55bdd75e29a4439ecc99702534405f
Bryan C Guner <bryan.guner@gmail.com>
2023-02-11 14:24:12 -0500

    login

--- 10effects-reducers-context/src/components/Login/Login.js
+++ 10effects-reducers-context/src/components/Login/Login.js
@@ -34,19 +34,7 @@ const Login = (props) => {
     };
   }, []);
 
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     console.log('Checking form validity!');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);
 
-  //   return () => {
-  //     console.log('CLEANUP');
-  //     clearTimeout(identifier);
-  //   };
-  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
