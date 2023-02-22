4976f255acf7b26900f4d593897cd8ba7320b2c1
Bryan C Guner <bryan.guner@gmail.com>
2023-02-22 14:32:21 -0500

    imported header into app

--- 11food-app/src/App.js
+++ 11food-app/src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
+import Header from './components/Layout/Header';
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Header>
     </div>
   );
 }
