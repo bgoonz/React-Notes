278ff3235fc22152e72763ba6b78ff23ef4338cf
Bryan C Guner <bryan.guner@gmail.com>
2023-02-22 14:54:49 -0500

    imported image into header

--- 11food-app/src/components/Layout/Header.js
+++ 11food-app/src/components/Layout/Header.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import classes from './Header.module.css';
-
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
 const Header = () => {
-    return (
-        <header className={ classes.header }>
-            <h1>ReactMeals</h1>
-            <button>Cart</button>
-        </header>
-    );
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img src={mealsImage} alt="a table of food"></img>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Header;
