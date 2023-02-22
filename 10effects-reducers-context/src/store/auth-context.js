9fff2d8be4a9bb57f3cc4341fd1e2927f366c9f5
Bryan C Guner <bryan.guner@gmail.com>
2023-02-22 11:21:22 -0500

    authcontextprovider component in auth-context.js

--- 10effects-reducers-context/src/store/auth-context.js
+++ 10effects-reducers-context/src/store/auth-context.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const AuthContext = React.createContext({
-  isLoggedIn: false
+  isLoggedIn: false,
+    onLogout: () => { },
+  onLogin: (email, password) => { },
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
