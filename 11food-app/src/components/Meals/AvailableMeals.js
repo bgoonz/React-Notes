b7eb5148bcc6de788438d1c05418ef1098d03460
Bryan C Guner <bryan.guner@gmail.com>
2023-02-23 10:06:02 -0500

    replaced li in mealsList in AvailableMeals.js with the MealItem component

--- 11food-app/src/components/Meals/AvailableMeals.js
+++ 11food-app/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 const DUMMY_MEALS = [
   {
@@ -30,7 +31,7 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+      return <MealItem key={ meal.id } name={ meal.name } description={ meal.description } price={meal.price } />;
   });
 
   return (
--- 11food-app/src/components/Meals/MealItem/MealItem.js
+++ 11food-app/src/components/Meals/MealItem/MealItem.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import classes from "./MealItem.module.css";
 const MealItem = ( props ) => {
-    
+    const price = `$${props.price.toFixed(2)}`
     
     return (
-        <li>
+        <li className={classes.meal}>
             <div>
                 <h3>{ props.name }</h3>
-                <div>{ props.description }</div>
-                <div>{ props.price }</div>
+                <div className={classes.description}>{ props.description }</div>
+                <div className={classes.price}>{ price }</div>
             </div>
             <div>
                 <button>Add to Cart</button>
