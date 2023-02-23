ed37aa2f4e103daab08895a3a9481724e00a0d12
Bryan C Guner <bryan.guner@gmail.com>
2023-02-23 16:18:57 -0500

    total amount in cart is now working dynamically

--- 11food-app/src/components/Cart/Cart.js
+++ 11food-app/src/components/Cart/Cart.js
@@ -1,10 +1,13 @@
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
-
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+    const totalAmount = `$${ cartCtx.totalAmount.toFixed( 2 ) }`;
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -15,7 +18,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+              <span>{totalAmount }</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
