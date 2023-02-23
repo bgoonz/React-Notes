32fbbf602f254a18dd5979fc906a67d3ee26a30d
Bryan C Guner <bryan.guner@gmail.com>
2023-02-23 13:17:44 -0500

    added CartContextProvider wrapper component... need to wrap all the child components in App.js to be done in next lecture entitled Using The Context

--- 11food-app/src/App.js
+++ 11food-app/src/App.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
@@ -14,13 +15,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
