f926178945a2248f0046d33acf227c25c5ba602d
Bryan C Guner <bryan.guner@gmail.com>
2023-02-25 18:06:11 -0500

    fetchMoviesHandler is working

--- 14http-database/src/App.js
+++ 14http-database/src/App.js
@@ -1,31 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
+    const [movies,setMovies] = useState([]);
+function fetchMoviesHandler() { 
+    fetch( 'https://swapi.dev/api/films/', {
+        method: 'GET',
+    } ).then( response => { 
+        return response.json().then( data => { 
+            const transformedMovies = data.results.map( movieData => {
+                return {
+                    id: movieData.episode_id,
+                    title: movieData.title,
+                    releaseDate: movieData.release_date,
+                    openingText: movieData.opening_crawl,
+                }
+            } )
+           setMovies(transformedMovies)
+        } )
+    } )
+}
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+              <button onClick={fetchMoviesHandler }>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
