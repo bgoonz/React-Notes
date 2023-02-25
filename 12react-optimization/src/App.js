3061b2c18481dd8aa55e2dbfd35443c87e619458
Bryan C Guner <bryan.guner@gmail.com>
2023-02-25 11:56:31 -0500

    toggleParahraphHandler

--- 12react-optimization/src/App.js
+++ 12react-optimization/src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-
+import React, {useState } from "react";
+import Button from "./components/UI/Button/Button";
 import "./App.css";
 
 function App() {
+    const [showParagraph,setShowParagraph] = useState(false);
+  const  toggleParagraphHandler = () => {
+        setShowParagraph(true);
+     };
+    
   return (
     <div className="app">
-      <h1>Hi there!</h1>
+          <h1>Hi there!</h1>
+          { showParagraph && <p>This is new!</p> }
+            <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
 }
