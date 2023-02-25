7bbd242f95f02acb3f48da06b3c551910a919392
Bryan C Guner <bryan.guner@gmail.com>
2023-02-25 11:13:52 -0500

    added useEffect to bump animate cart button whenever item is added to cart with a setTimeout inside to reset the animation class after 300ms

--- 11food-app/src/components/Layout/HeaderCartButton.js
+++ 11food-app/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect,useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-const HeaderCartButton = (props) => {
-  const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+const HeaderCartButton = ( props ) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const cartCtx = useContext( CartContext );
+        const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
-  }, 0);
+  }, 0 );
+
+    const btnClasses = `${ classes.button } ${btnIsHighlighted?classes.bump:'' }`;
+    useEffect( () => {
+        if ( items.length === 0 ) { 
+            return;
+        }
+        
+        setBtnIsHighlighted( true );
+        const timer = setTimeout( () => {
+            setBtnIsHighlighted( false );
+        }, 300 );
+        return () => {
+            clearTimeout( timer );
+        }
+        
+     }, [ items ] );
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span>
         <CartIcon className={classes.icon} />
       </span>
