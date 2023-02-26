809b13fb6744ac5d435cf201a15accbd448d2f0c
Bryan C Guner <bryan.guner@gmail.com>
2023-02-26 08:19:15 -0500

    use-counter hook

---
+++ 15custom-hooks/src/hooks/use-counter.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+const useCounter = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+};
+
+export default useCounter;
