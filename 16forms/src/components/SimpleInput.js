d73b3001dbc1895e4820073e163e565ef58e180d
Bryan C Guner <bryan.guner@gmail.com>
2023-03-04 14:49:04 -0500

    removed enteredNameIsValid state

--- 16forms/src/components/SimpleInput.js
+++ 16forms/src/components/SimpleInput.js
@@ -1,40 +1,30 @@
-import {  useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      console.log("Input is valid!");
-    }
-  });
-
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-    if (event.target.value.trim() !== "") {
-      setEnteredNameIsValid(true);
-    }
   };
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-    }
   };
   const formSubmissionHandler = (event) => {
     setEnteredNameTouched(true);
     event.preventDefault();
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-    setEnteredNameIsValid(true);
+
     console.log(enteredName);
-    setEnteredName("");
+      setEnteredName( "" );
+        setEnteredNameTouched( false );
   };
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
     : "form-control";
@@ -44,7 +34,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
- 
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
