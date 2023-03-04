e2d67485bb23593abcbde16a998ea7bc2ac2eba9
Bryan C Guner <bryan.guner@gmail.com>
2023-03-04 13:26:13 -0500

    nameInputClasses to dynamically style invalid input

--- 16forms/src/components/SimpleInput.js
+++ 16forms/src/components/SimpleInput.js
@@ -3,24 +3,28 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
   const formSubmissionHandler = (event) => {
-      event.preventDefault();
-      if ( enteredName.trim() === '' ) {
-          return;
-      }
+    event.preventDefault();
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
     console.log(enteredName);
     // const enteredValue = nameInputRef.current.value;
     // console.log(enteredValue);
     setEnteredName("");
   };
+    const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+          <div className={nameInputClasses }>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -29,6 +33,9 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty!</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
