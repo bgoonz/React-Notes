061381c7781358cb8fa0190dd9308c6035638f43
Bryan C Guner <bryan.guner@gmail.com>
2023-03-07 14:20:46 -0500

    beginning of adding form validation

--- 17http-forms-food-order/src/components/Cart/Checkout.js
+++ 17http-forms-food-order/src/components/Cart/Checkout.js
@@ -1,27 +1,44 @@
+import { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
+const isEmpty = (value) => value.trim() === "";
+const isNotFiveChars = (value) => value.trim().length !== 5;
+
 const Checkout = (props) => {
+  const enteredName = useRef();
+  const enteredStreet = useRef();
+  const enteredPostalCode = useRef();
+  const enteredCity = useRef();
+    const enteredNameIsValid = !isEmpty( enteredName );
+    const enteredStreetIsValid = !isEmpty( enteredStreet );
+    const enteredPostalCodeIsValid = !isNotFiveChars( enteredPostalCode );
+    const enteredCityIsValid = !isEmpty( enteredCity );
+    
   const confirmHandler = (event) => {
     event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
   };
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalCodeInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
