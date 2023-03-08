8ef08b4f78451ba873a9fb0a08c2560470c870d5
Bryan C Guner <bryan.guner@gmail.com>
2023-03-08 13:14:31 -0500

    more redux basics

--- 18redux-from-scratch/redux-demo.js
+++ 18redux-from-scratch/redux-demo.js
@@ -1,17 +1,27 @@
+const redux = require("redux");
 
-const redux = require( 'redux' );
-
-const counterReducer = (state,action) => { 
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
     return {
-        counter: state.counter + 1
+      counter: state.counter + 1,
+    };
     }
+    if ( action.type === "decrement" ) {
+        return {
+            counter: state.counter - 1
+        }
+    }
+  return state;
 };
 const store = redux.createStore(counterReducer);
 
-
-const counterSubscriber = () => { 
-    const latestState = store.getState();
-    console.log(latestState);
+// console.log(store.getState());
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
 };
 
 store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
