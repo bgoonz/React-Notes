9dff67857e14a76c9d9120fed2fd415e3b2940b0
Bryan C Guner <bryan.guner@gmail.com>
2023-03-09 08:45:17 -0500

    wired counter.js to store and inctement and decrement buttons are functional

--- 18redux/src/components/Counter.js
+++ 18redux/src/components/Counter.js
@@ -1,16 +1,24 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-    const counter = useSelector( state => state.counter );
-    const toggleCounterHandler = () => {
-      
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const toggleCounterHandler = () => {};
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
   };
-
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
