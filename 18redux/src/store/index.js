a29952042a5b3fdc9b5bbf756574f1952987c670
Bryan C Guner <bryan.guner@gmail.com>
2023-03-09 17:07:41 -0500

    243 connecting redux toolkit state complete

--- 18redux/src/store/index.js
+++ 18redux/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 const initialState = { counter: 0, showCounter: true };
-createSlice({
+const counterSlice=createSlice({
   name: "counter",
   initialState,
   reducers: {
@@ -19,36 +19,11 @@ createSlice({
     },
   },
 });
-//objects returned in the reducer to not change the existing state... they overwrite it
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
 
-  return state;
-};
 
-const store = createStore(counterReducer);
+const store = configureStore( {
+    reducer: counterSlice.reducer
+    
+} );
 
 export default store;
