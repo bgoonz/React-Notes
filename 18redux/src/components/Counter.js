1c13345f3bb89d9b819a907969f3271932fb4b6b
Bryan C Guner <bryan.guner@gmail.com>
2023-03-09 08:24:14 -0500

    done with 235 using redux data in react components

--- 18redux/src/components/Counter.js
+++ 18redux/src/components/Counter.js
@@ -1,12 +1,16 @@
 import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+    const counter = useSelector( state => state.counter );
+    const toggleCounterHandler = () => {
+      
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
