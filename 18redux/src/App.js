973a61b2ab70a18fdf78d27c3fb116949bdc30bb
Bryan C Guner <bryan.guner@gmail.com>
2023-03-10 08:58:06 -0500

    246 reading and dispathing from a new slice complete

--- 18redux/src/App.js
+++ 18redux/src/App.js
@@ -1,13 +1,15 @@
 import Counter from "./components/Counter";
+import { useSelector } from "react-redux";
 import { Fragment } from "react";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
-
+import UserProfile from "./components/UserProfile";
 function App() {
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <Fragment>
       <Header />
-      <Auth />
+          { !isAuth ?< Auth />: <UserProfile />}
       <Counter />
     </Fragment>
   );
--- 18redux/src/components/Header.js
+++ 18redux/src/components/Header.js
@@ -1,22 +1,30 @@
 import classes from "./Header.module.css";
-
+import { useSelector,useDispatch } from 'react-redux';
+import { authActions } from '../store/index';
 const Header = () => {
+    const dispatch = useDispatch();
+    const logoutHandler = (event) => {
+        dispatch( authActions.logout() );
+    }
+    
+    const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">My Products</a>
-          </li>
-          <li>
-            <a href="/">My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+          <h1>Redux Auth</h1>
+          { isAuth &&
+              <nav>
+                  <ul>
+                      <li>
+                          <a href="/">My Products</a>
+                      </li>
+                      <li>
+                          <a href="/">My Sales</a>
+                      </li>
+                      <li>
+                          <button onClick={logoutHandler}>Logout</button>
+                      </li>
+                  </ul>
+              </nav> }
     </header>
   );
 };
