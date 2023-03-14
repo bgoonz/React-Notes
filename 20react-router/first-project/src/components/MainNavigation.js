ab70f8526d1b1ebef105994a25b1b009c8e9b21f
Bryan C Guner <bryan.guner@gmail.com>
2023-03-14 15:13:05 -0400

    276 working with Navigation Links... Done

--- 20react-router/first-project/src/components/MainNavigation.js
+++ 20react-router/first-project/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 function MainNavigation() {
   return (
@@ -6,10 +6,25 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end={true}
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
