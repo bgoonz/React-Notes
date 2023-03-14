68f9d6cc83184144ad2eeff3b2aa53997523671e
Bryan C Guner <bryan.guner@gmail.com>
2023-03-14 15:00:55 -0400

    error page

--- 20react-router/first-project/src/App.js
+++ 20react-router/first-project/src/App.js
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
--- 20react-router/first-project/src/pages/Root.js
+++ 20react-router/first-project/src/pages/Root.js
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
-import classes from "./Root.module.css";
+
 function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <main className={classes.content}>
+      <main >
         <Outlet />
       </main>
     </>
