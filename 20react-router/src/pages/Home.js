c2ce593ddd8dba684fc9b492b58bb94d1a540622
Bryan C Guner <bryan.guner@gmail.com>
2023-03-14 12:38:40 -0400

    link component from react router dom

--- 20react-router/src/pages/Home.js
+++ 20react-router/src/pages/Home.js
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 function HomePage() {
   return (
     <div>
-      <h1>The Home Page</h1>
+          <h1>The Home Page</h1>
+          <p>Go to <Link to="/products">the list of products</Link></p>
     </div>
   );
 }
