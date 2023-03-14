4154ff41c7bdff94d6e50282e15e2c8c1c7c881c
Bryan C Guner <bryan.guner@gmail.com>
2023-03-14 15:17:32 -0400

    navigating programatically ... 277 Done

--- 20react-router/first-project/src/pages/Home.js
+++ 20react-router/first-project/src/pages/Home.js
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
-import MainNavigation from "../components/MainNavigation";
+import { Link,useNavigate } from "react-router-dom";
+
 function HomePage() {
+    const navigate = useNavigate();
+    function navigateHandler() {
+        navigate('/products')
+    }
   return (
     <div>
       <h1>The Home Page</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>
-      </p>
+          </p>
+          <p>
+              <button onClick={navigateHandler}>Go to Products</button>
+          </p>
     </div>
   );
 }
