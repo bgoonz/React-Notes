680ccbc7500f7b56aa76492b69465bb2301fb7a6
Bryan C Guner <bryan.guner@gmail.com>
2023-03-14 12:22:03 -0400

    adding another route

--- 20react-router/src/App.js
+++ 20react-router/src/App.js
@@ -1,12 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
-
+import ProductsPage from "./pages/Products";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
   },
-  {},
+  { path: "/products", element: <ProductsPage /> },
 ]);
 function App() {
   return (
---
+++ 20react-router/src/pages/Products.js
@@ -0,0 +1,10 @@
+
+
+function ProductsPage() {
+  return (
+    <div>
+      <h1>The Products Page</h1>
+    </div>
+  );
+}
+export default ProductsPage;
