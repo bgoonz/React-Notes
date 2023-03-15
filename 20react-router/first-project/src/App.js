9768667bb57c0b882f40c0b8cc0fa8993642d06c
Bryan C Guner <bryan.guner@gmail.com>
2023-03-15 11:52:55 -0400

    index routes 280

--- 20react-router/first-project/src/App.js
+++ 20react-router/first-project/src/App.js
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/",
+        index: true,
         element: <HomePage />,
       },
       { path: "/products", element: <ProductsPage /> },
