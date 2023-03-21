8250f311f9584dfb4b47ca3a71daa8b2e3902aa0
Bryan C Guner <bryan.guner@gmail.com>
2023-03-21 15:53:55 -0400

    315 validating user input & outputting validation errors

--- 21authentication/frontend/src/components/AuthForm.js
+++ 21authentication/frontend/src/components/AuthForm.js
@@ -1,14 +1,27 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import { Form, Link, useSearchParams, useActionData, useNavigation } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
+    const data = useActionData();
+    //navigation has a state that lets us know if we are currently submitting the form
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state ==='submitting'
+    
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((error) => {
+                return <li style={ { color: 'red' } } key={error}>{error}</li>;
+            })}
+          </ul>
+              ) }
+              { data && data.message && <p style={{color:'red'}}>{ data.message }</p> }
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -21,7 +34,7 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>{isSubmitting?'Submitting...':'Save'}</button>
         </div>
       </Form>
     </>
