1a018c94d259722ee7057a9eea386e8011704a24
Bryan C Guner <bryan.guner@gmail.com>
2023-03-23 15:43:00 -0400

    useRouter

--- 23nextjs-intro/pages/news/[newsId].js
+++ 23nextjs-intro/pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import {useRouter} from 'next/router';
+// useRouter is a custom react hook that returns an object with the route information
 function DetailPage() {
+    const router = useRouter();
+    const newsId = router.query.newsId;
+    //send a request to the backend API to fetch news item with newsId
+    
   return <h1>The Detail Page</h1>;
 }
 
