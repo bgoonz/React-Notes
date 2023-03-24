b1d9f4a4567fef797d5a3ed1f917ec2bcdfa9f6a
Bryan C Guner <bryan.guner@gmail.com>
2023-03-24 11:50:37 -0400

    next Link component end of 343

--- 23nextjs-intro/pages/news/index.js
+++ 23nextjs-intro/pages/news/index.js
@@ -1,9 +1,17 @@
 /*https://my-domain/news */
-
+import Link from 'next/link'
 function NewsPage() {
   return (
     <>
       <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">Nextjs</Link>
+        </li>
+        <li>
+          <Link href="/news/reactjs">React</Link>
+        </li>
+      </ul>
     </>
   );
 }
