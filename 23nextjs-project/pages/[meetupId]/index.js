76191d9304f355318b66f2a7dd8658c965b93d29
Bryan C Guner <bryan.guner@gmail.com>
2023-03-26 14:29:39 -0400

    dynamic meta info

--- 23nextjs-project/pages/[meetupId]/index.js
+++ 23nextjs-project/pages/[meetupId]/index.js
@@ -1,13 +1,23 @@
+import { MongoClient, ObjectId } from 'mongodb';
+import { Fragment } from 'react';
+import Head from 'next/head';
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
-import { MongoClient, ObjectId } from "mongodb";
-function MetupDetails(props) {
+
+function MeetupDetails(props) {
   return (
-    <MeetupDetail
-      image={props.meetupData.image}
-      title={props.meetupData.title}
-      address={props.meetupData.address}
-      description={props.meetupData.description}
-    />
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name='description' content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
   );
 }
 export async function getStaticPaths() {
@@ -52,4 +62,4 @@ export async function getStaticProps(context) {
   };
 }
 
-export default MetupDetails;
+export default MeetupDetails;
--- 23nextjs-project/pages/index.js
+++ 23nextjs-project/pages/index.js
@@ -1,8 +1,19 @@
 import MeetupList from "../components/meetups/MeetupList";
 import { MongoClient } from "mongodb";
-
+import Head from "next/head";
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        ></meta>
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
 }
 
 export async function getStaticProps() {
