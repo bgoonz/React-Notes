659a809168dc133bd6628900b4f2b30a2efe0625
Bryan C Guner <bryan.guner@gmail.com>
2023-03-27 16:35:01 -0400

    replacing redux with oontext and custom hooks

---
+++ 25replacing-redux-with-context/src/components/UI/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Card.css";
+
+const Card = (props) => {
+  return (
+    <div className="card" style={props.style}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
