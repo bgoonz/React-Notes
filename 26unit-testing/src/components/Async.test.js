74470df479caa7a553e666a69cfdeea9b6801ee3
Bryan C Guner <bryan.guner@gmail.com>
2023-03-29 09:04:44 -0400

    410 jest mocks for simulating api requests

--- 26unit-testing/src/components/Async.test.js
+++ 26unit-testing/src/components/Async.test.js
@@ -3,9 +3,18 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders posts if request succeeds", async () => {
+    //jest.fn() is a mock function
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     render(<Async />);
-      const listItems = await screen.findAllByRole( "listitem", { exact: false } , { timeout: 5000 } );
-      
-        expect( listItems ).not.toHaveLength( 0 );
+    const listItems = await screen.findAllByRole(
+      "listitem",
+      { exact: false },
+      { timeout: 5000 }
+    );
+
+    expect(listItems).not.toHaveLength(0);
   });
 });
